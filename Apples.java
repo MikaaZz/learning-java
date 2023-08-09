6a9f2a6eaf6e101057ccdd73d2bbe2a569a0a725
mikhael zimmermann <mikhael928@gmail.com>
2023-08-09 20:18:20 -0300

    Creating variables

---
+++ Apples.java
@@ -0,0 +1,9 @@
+public class Apples {
+  public static void main(String[] args) {
+    int numberOfApples = 15;
+    double pricePerApple = 0.29, totalCost = numberOfApples * pricePerApple;;
+
+    System.out.println(numberOfApples + " apples at $" + pricePerApple + " apiece cost $" + totalCost);
+
+  } // End main
+}// End Apples
